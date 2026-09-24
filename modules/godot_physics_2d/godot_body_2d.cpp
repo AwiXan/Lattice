godotengine/godot

Fix CharacterBody2D BVH performance issue
Character bodies (PS2DE::BODY_MODE_KINEMATIC) were incorrectly expanding
their AABBs to incorporate next frame motion. This should only happen
if CCD is turned on. If CCD is not turned on, all the extra broad phase
hits are wasted, since narrow phase will not consider motion unless
CCD is on.

modules/godot_physics_2d/godot_body_2d.cpp
@@ -565,8 +565,7 @@ void GodotBody2D::integrate_forces(real_t p_step) {
 		real_t rot = new_transform.get_rotation() - get_transform().get_rotation();
 		angular_velocity = constant_angular_velocity + std::remainder(rot, 2.0 * Math::PI) / p_step;
 
-		do_motion = true;
-
+		do_motion = continuous_cd_mode != PS2DE::CCD_MODE_DISABLED;
 	} else {
 		if (!omit_force_integration) {
 			//overridden by direct state query
@@ -624,7 +623,7 @@ void GodotBody2D::integrate_velocities(real_t p_step) {
 	}
 
 	if (mode == PS2DE::BODY_MODE_KINEMATIC) {
-		_set_transform(new_transform, false);
+		_set_transform(new_transform, continuous_cd_mode == PS2DE::CCD_MODE_DISABLED);
 		_set_inv_transform(new_transform.affine_inverse());
 		if (contacts.is_empty() && linear_velocity == Vector2() && angular_velocity == 0) {
 			set_active(false); //stopped moving, deactivate

AwiXan/Lattice

(cherry picked from commit 88033486abb0f5b780505750c35f5b0ce62ea1bd)

modules/godot_physics_2d/godot_body_2d.cpp
@@ -565,8 +565,7 @@ void GodotBody2D::integrate_forces(real_t p_step) {
 		real_t rot = new_transform.get_rotation() - get_transform().get_rotation();
 		angular_velocity = constant_angular_velocity + std::remainder(rot, 2.0 * Math::PI) / p_step;
 
-		do_motion = true;
-
+		do_motion = continuous_cd_mode != PhysicsServer2D::CCD_MODE_DISABLED;
 	} else {
 		if (!omit_force_integration) {
 			//overridden by direct state query
@@ -624,7 +623,7 @@ void GodotBody2D::integrate_velocities(real_t p_step) {
 	}
 
 	if (mode == PhysicsServer2D::BODY_MODE_KINEMATIC) {
-		_set_transform(new_transform, false);
+		_set_transform(new_transform, continuous_cd_mode == PhysicsServer2D::CCD_MODE_DISABLED);
 		_set_inv_transform(new_transform.affine_inverse());
 		if (contacts.is_empty() && linear_velocity == Vector2() && angular_velocity == 0) {
 			set_active(false); //stopped moving, deactivate