godotengine/godot

Add motion blur support
Graphics part by @HydrogenC, editor part mainly by @AR-DEV-1 & docs mainly by @sphynx-owner

Co-Authored-By: AR <ardev1.deverson@proton.me>
Co-Authored-By: sphynx-owner <61445300+sphynx-owner@users.noreply.github.com>

servers/rendering/renderer_rd/renderer_scene_render_rd.cpp
@@ -547,6 +547,22 @@ void RendererSceneRenderRD::_render_buffers_post_process_and_tonemap(const Rende
 		RD::get_singleton()->draw_command_end_label();
 	}
 
+	bool using_motion_blur = RSG::camera_attributes->camera_attributes_uses_motion_blur(p_render_data->camera_attributes);
+
+	if (using_motion_blur && !can_use_storage) {
+		WARN_PRINT_ONCE("Motion blur requires storage support in shader. Disabling motion blur.");
+		using_motion_blur = false;
+	}
+
+	if (using_motion_blur && !RSG::camera_attributes->camera_attributes_get_motion_blur_show_in_editor() && Engine::get_singleton()->is_editor_hint()) {
+		using_motion_blur = false;
+	}
+
+	if (can_use_effects && using_motion_blur) {
+		RENDER_TIMESTAMP("Motion Blur");
+		motion_blur->motion_blur_compute(rb, p_render_data->camera_attributes, p_render_data->scene_data, p_render_data->transparent_bg, time_step, copy_effects);
+	}
+
 	float auto_exposure_scale = 1.0;
 
 	if (can_use_effects && RSG::camera_attributes->camera_attributes_uses_auto_exposure(p_render_data->camera_attributes)) {
@@ -1721,6 +1737,11 @@ void RendererSceneRenderRD::init() {
 		RendererRD::Fog::get_singleton()->init_fog_shader(RendererRD::LightStorage::get_singleton()->get_max_directional_lights(), get_roughness_layers(), is_using_radiance_octmap_array());
 	}
 
+	RSG::camera_attributes->camera_attributes_set_motion_blur_framerate_mode(RS::MotionBlurFramerateMode(int(GLOBAL_GET("rendering/camera/motion_blur/motion_blur_framerate_mode"))), int(GLOBAL_GET("rendering/camera/motion_blur/motion_blur_reference_framerate")));
+	RSG::camera_attributes->camera_attributes_set_motion_blur_show_in_editor(bool(GLOBAL_GET("rendering/camera/motion_blur/motion_blur_show_in_editor")));
+	RSG::camera_attributes->camera_attributes_set_motion_blur_quality(RS::MotionBlurQuality(int(GLOBAL_GET("rendering/camera/motion_blur/motion_blur_quality"))));
+	RSG::camera_attributes->camera_attributes_set_motion_blur_tile_size(RS::MotionBlurTileSize(int(GLOBAL_GET("rendering/camera/motion_blur/motion_blur_tile_size"))));
+
 	RSG::camera_attributes->camera_attributes_set_dof_blur_bokeh_shape(RSE::DOFBokehShape(int(GLOBAL_GET("rendering/camera/depth_of_field/depth_of_field_bokeh_shape"))));
 	RSG::camera_attributes->camera_attributes_set_dof_blur_quality(RSE::DOFBlurQuality(int(GLOBAL_GET("rendering/camera/depth_of_field/depth_of_field_bokeh_quality"))), GLOBAL_GET("rendering/camera/depth_of_field/depth_of_field_use_jitter"));
 	use_physical_light_units = GLOBAL_GET("rendering/lights_and_shadows/use_physical_light_units");
@@ -1768,6 +1789,7 @@ void RendererSceneRenderRD::init() {
 	}
 
 	bokeh_dof = memnew(RendererRD::BokehDOF(!can_use_storage));
+	motion_blur = memnew(RendererRD::MotionBlur(RSG::camera_attributes->camera_attributes_get_motion_blur_tile_size()));
 	copy_effects = memnew(RendererRD::CopyEffects(raster_effects));
 	debug_effects = memnew(RendererRD::DebugEffects);
 	luminance = memnew(RendererRD::Luminance(!can_use_storage));
@@ -1792,6 +1814,9 @@ RendererSceneRenderRD::~RendererSceneRenderRD() {
 
 	if (bokeh_dof) {
 		memdelete(bokeh_dof);
+	}
+	if (motion_blur) {
+		memdelete(motion_blur);
 	}
 	if (copy_effects) {
 		memdelete(copy_effects);

servers/rendering/storage/camera_attributes_storage.h
@@ -49,6 +49,15 @@ private:
 		float auto_exposure_scale = 1.0;
 		uint64_t auto_exposure_version = 0;
 
+		bool motion_blur_enabled = false;
+		float motion_blur_intensity = 1.0;
+		bool motion_blur_clamp_velocities_to_tile = true;
+		float motion_blur_object_velocity_multiplier = 1.0;
+		float motion_blur_movement_velocity_multiplier = 1.0;
+		float motion_blur_rotation_velocity_multiplier = 1.0;
+		float motion_blur_velocity_lower_threshold = 0.0;
+		float motion_blur_velocity_upper_threshold = 0.0;
+
 		bool dof_blur_far_enabled = false;
 		float dof_blur_far_distance = 10;
 		float dof_blur_far_transition = 5;
@@ -57,6 +66,12 @@ private:
 		float dof_blur_near_transition = 1;
 		float dof_blur_amount = 0.1;
 	};
+
+	RS::MotionBlurFramerateMode motion_blur_framerate_mode = RS::MOTION_BLUR_FRAMERATE_MODE_CAPPED;
+	int motion_blur_reference_framerate = 30;
+	bool motion_blur_show_in_editor = true;
+	RS::MotionBlurQuality motion_blur_quality = RS::MOTION_BLUR_QUALITY_MEDIUM;
+	RS::MotionBlurTileSize motion_blur_tile_size = RS::MOTION_BLUR_TILE_SIZE_MEDIUM;
 
 	RSE::DOFBlurQuality dof_blur_quality = RSE::DOF_BLUR_QUALITY_MEDIUM;
 	RSE::DOFBokehShape dof_blur_bokeh_shape = RSE::DOF_BOKEH_HEXAGON;
@@ -77,6 +92,26 @@ public:
 	RID camera_attributes_allocate();
 	void camera_attributes_initialize(RID p_rid);
 	void camera_attributes_free(RID p_rid);
+
+	void camera_attributes_set_motion_blur_framerate_mode(RS::MotionBlurFramerateMode p_mode, int p_reference_framerate);
+	void camera_attributes_set_motion_blur_show_in_editor(bool p_enabled);
+	void camera_attributes_set_motion_blur_quality(RS::MotionBlurQuality p_quality);
+	void camera_attributes_set_motion_blur_tile_size(RS::MotionBlurTileSize p_tile_size);
+
+	void camera_attributes_set_motion_blur(RID p_camera_attributes, bool p_enable, float p_intensity, bool p_clamp_velocities_to_tile, float p_object_velocity_multiplier, float p_movement_velocity_multiplier, float p_rotation_velocity_multiplier, float p_velocity_lower_threshold, float p_velocity_upper_threshold);
+	float camera_attributes_get_motion_blur_intensity(RID p_camera_attributes);
+	bool camera_attributes_get_motion_blur_clamp_velocities_to_tile(RID p_camera_attributes);
+	float camera_attributes_get_motion_blur_object_velocity_multiplier(RID p_camera_attributes);
+	float camera_attributes_get_motion_blur_movement_velocity_multiplier(RID p_camera_attributes);
+	float camera_attributes_get_motion_blur_rotation_velocity_multiplier(RID p_camera_attributes);
+	float camera_attributes_get_motion_blur_velocity_lower_threshold(RID p_camera_attributes);
+	float camera_attributes_get_motion_blur_velocity_upper_threshold(RID p_camera_attributes);
+
+	_FORCE_INLINE_ bool camera_attributes_uses_motion_blur(RID p_camera_attributes) {
+		CameraAttributes *cam_attributes = camera_attributes_owner.get_or_null(p_camera_attributes);
+
+		return cam_attributes && cam_attributes->motion_blur_enabled && cam_attributes->motion_blur_intensity > 0.0;
+	}
 
 	void camera_attributes_set_dof_blur_quality(RSE::DOFBlurQuality p_quality, bool p_use_jitter);
 	void camera_attributes_set_dof_blur_bokeh_shape(RSE::DOFBokehShape p_shape);
@@ -110,6 +145,26 @@ public:
 		CameraAttributes *cam_attributes = camera_attributes_owner.get_or_null(p_camera_attributes);
 
 		return cam_attributes && cam_attributes->use_auto_exposure;
+	}
+
+	_FORCE_INLINE_ RS::MotionBlurFramerateMode camera_attributes_get_motion_blur_framerate_mode() {
+		return motion_blur_framerate_mode;
+	}
+
+	_FORCE_INLINE_ int camera_attributes_get_motion_blur_reference_framerate() {
+		return motion_blur_reference_framerate;
+	}
+
+	_FORCE_INLINE_ bool camera_attributes_get_motion_blur_show_in_editor() {
+		return motion_blur_show_in_editor;
+	}
+
+	_FORCE_INLINE_ RS::MotionBlurQuality camera_attributes_get_motion_blur_quality() {
+		return motion_blur_quality;
+	}
+
+	_FORCE_INLINE_ RS::MotionBlurTileSize camera_attributes_get_motion_blur_tile_size() {
+		return motion_blur_tile_size;
 	}
 
 	_FORCE_INLINE_ RSE::DOFBlurQuality camera_attributes_get_dof_blur_quality() {

AwiXan/Lattice

Co-Authored-By: DaveTheEggman <ardev1.deverson@proton.me>
Co-Authored-By: A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
Co-authored-by: Hugo Locurcio <hugo.locurcio@hugo.pro>
Co-Authored-By: passivestar <60579014+passivestar@users.noreply.github.com>
(cherry picked from commit dc50f2966a89e612841071aaede605c13c5ba38e)

servers/rendering/renderer_rd/renderer_scene_render_rd.cpp
@@ -552,6 +552,18 @@ void RendererSceneRenderRD::_render_buffers_post_process_and_tonemap(const Rende
 		RD::get_singleton()->draw_command_end_label();
 	}
 
+	bool using_motion_blur = RSG::camera_attributes->camera_attributes_uses_motion_blur(p_render_data->camera_attributes);
+
+	if (using_motion_blur && !can_use_storage) {
+		WARN_PRINT_ONCE("Motion blur requires storage support in shader. Disabling motion blur.");
+		using_motion_blur = false;
+	}
+
+	if (can_use_effects && using_motion_blur) {
+		RENDER_TIMESTAMP("Motion Blur");
+		motion_blur->motion_blur_compute(rb, p_render_data->camera_attributes, p_render_data->scene_data, p_render_data->transparent_bg, time_step, copy_effects);
+	}
+
 	float auto_exposure_scale = 1.0;
 
 	if (can_use_effects && RSG::camera_attributes->camera_attributes_uses_auto_exposure(p_render_data->camera_attributes)) {
@@ -1825,6 +1837,10 @@ void RendererSceneRenderRD::init() {
 		RendererRD::Fog::get_singleton()->init_fog_shader(RendererRD::LightStorage::get_singleton()->get_max_directional_lights(), get_roughness_layers(), is_using_radiance_octmap_array());
 	}
 
+	RSG::camera_attributes->camera_attributes_set_motion_blur_framerate_mode(RSE::MotionBlurFramerateMode(int(GLOBAL_GET("rendering/camera/motion_blur/motion_blur_framerate_mode"))), int(GLOBAL_GET("rendering/camera/motion_blur/motion_blur_reference_framerate")));
+	RSG::camera_attributes->camera_attributes_set_motion_blur_quality(RSE::MotionBlurQuality(int(GLOBAL_GET("rendering/camera/motion_blur/motion_blur_quality"))));
+	RSG::camera_attributes->camera_attributes_set_motion_blur_tile_size(RSE::MotionBlurTileSize(int(GLOBAL_GET("rendering/camera/motion_blur/motion_blur_tile_size"))));
+
 	RSG::camera_attributes->camera_attributes_set_dof_blur_bokeh_shape(RSE::DOFBokehShape(int(GLOBAL_GET("rendering/camera/depth_of_field/depth_of_field_bokeh_shape"))));
 	RSG::camera_attributes->camera_attributes_set_dof_blur_quality(RSE::DOFBlurQuality(int(GLOBAL_GET("rendering/camera/depth_of_field/depth_of_field_bokeh_quality"))), GLOBAL_GET("rendering/camera/depth_of_field/depth_of_field_use_jitter"));
 	use_physical_light_units = GLOBAL_GET("rendering/lights_and_shadows/use_physical_light_units");
@@ -1873,6 +1889,7 @@ void RendererSceneRenderRD::init() {
 	}
 
 	bokeh_dof = memnew(RendererRD::BokehDOF(!can_use_storage));
+	motion_blur = memnew(RendererRD::MotionBlur(RSG::camera_attributes->camera_attributes_get_motion_blur_tile_size()));
 	copy_effects = memnew(RendererRD::CopyEffects(raster_effects));
 	debug_effects = memnew(RendererRD::DebugEffects);
 	luminance = memnew(RendererRD::Luminance(!can_use_storage));
@@ -1897,6 +1914,9 @@ RendererSceneRenderRD::~RendererSceneRenderRD() {
 
 	if (bokeh_dof) {
 		memdelete(bokeh_dof);
+	}
+	if (motion_blur) {
+		memdelete(motion_blur);
 	}
 	if (copy_effects) {
 		memdelete(copy_effects);

servers/rendering/storage/camera_attributes_storage.h
@@ -49,6 +49,15 @@ private:
 		float auto_exposure_scale = 1.0;
 		uint64_t auto_exposure_version = 0;
 
+		bool motion_blur_enabled = false;
+		float motion_blur_intensity = 1.0;
+		bool motion_blur_clamp_velocities_to_tile = true;
+		float motion_blur_object_velocity_multiplier = 1.0;
+		float motion_blur_movement_velocity_multiplier = 1.0;
+		float motion_blur_rotation_velocity_multiplier = 1.0;
+		float motion_blur_velocity_lower_threshold = 0.0;
+		float motion_blur_velocity_upper_threshold = 0.0;
+
 		bool dof_blur_far_enabled = false;
 		float dof_blur_far_distance = 10;
 		float dof_blur_far_transition = 5;
@@ -57,6 +66,12 @@ private:
 		float dof_blur_near_transition = 1;
 		float dof_blur_amount = 0.1;
 	};
+
+	bool motion_blur_show_in_editor = true;
+	RSE::MotionBlurFramerateMode motion_blur_framerate_mode = RSE::MOTION_BLUR_FRAMERATE_MODE_CAPPED;
+	int motion_blur_reference_framerate = 30;
+	RSE::MotionBlurQuality motion_blur_quality = RSE::MOTION_BLUR_QUALITY_MEDIUM;
+	RSE::MotionBlurTileSize motion_blur_tile_size = RSE::MOTION_BLUR_TILE_SIZE_MEDIUM;
 
 	RSE::DOFBlurQuality dof_blur_quality = RSE::DOF_BLUR_QUALITY_MEDIUM;
 	RSE::DOFBokehShape dof_blur_bokeh_shape = RSE::DOF_BOKEH_HEXAGON;
@@ -77,6 +92,26 @@ public:
 	RID camera_attributes_allocate();
 	void camera_attributes_initialize(RID p_rid);
 	void camera_attributes_free(RID p_rid);
+
+	void camera_attributes_set_motion_blur_show_in_editor(bool p_enabled);
+	void camera_attributes_set_motion_blur_framerate_mode(RSE::MotionBlurFramerateMode p_mode, int p_reference_framerate);
+	void camera_attributes_set_motion_blur_quality(RSE::MotionBlurQuality p_quality);
+	void camera_attributes_set_motion_blur_tile_size(RSE::MotionBlurTileSize p_tile_size);
+
+	void camera_attributes_set_motion_blur(RID p_camera_attributes, bool p_enable, float p_intensity, bool p_clamp_velocities_to_tile, float p_object_velocity_multiplier, float p_movement_velocity_multiplier, float p_rotation_velocity_multiplier, float p_velocity_lower_threshold, float p_velocity_upper_threshold);
+	float camera_attributes_get_motion_blur_intensity(RID p_camera_attributes);
+	bool camera_attributes_get_motion_blur_clamp_velocities_to_tile(RID p_camera_attributes);
+	float camera_attributes_get_motion_blur_object_velocity_multiplier(RID p_camera_attributes);
+	float camera_attributes_get_motion_blur_movement_velocity_multiplier(RID p_camera_attributes);
+	float camera_attributes_get_motion_blur_rotation_velocity_multiplier(RID p_camera_attributes);
+	float camera_attributes_get_motion_blur_velocity_lower_threshold(RID p_camera_attributes);
+	float camera_attributes_get_motion_blur_velocity_upper_threshold(RID p_camera_attributes);
+
+	_FORCE_INLINE_ bool camera_attributes_uses_motion_blur(RID p_camera_attributes) {
+		CameraAttributes *cam_attributes = camera_attributes_owner.get_or_null(p_camera_attributes);
+
+		return cam_attributes && cam_attributes->motion_blur_enabled && cam_attributes->motion_blur_intensity > 0.0;
+	}
 
 	void camera_attributes_set_dof_blur_quality(RSE::DOFBlurQuality p_quality, bool p_use_jitter);
 	void camera_attributes_set_dof_blur_bokeh_shape(RSE::DOFBokehShape p_shape);
@@ -110,6 +145,26 @@ public:
 		CameraAttributes *cam_attributes = camera_attributes_owner.get_or_null(p_camera_attributes);
 
 		return cam_attributes && cam_attributes->use_auto_exposure;
+	}
+
+	_FORCE_INLINE_ bool camera_attributes_get_motion_blur_show_in_editor() {
+		return motion_blur_show_in_editor;
+	}
+
+	_FORCE_INLINE_ RSE::MotionBlurFramerateMode camera_attributes_get_motion_blur_framerate_mode() {
+		return motion_blur_framerate_mode;
+	}
+
+	_FORCE_INLINE_ int camera_attributes_get_motion_blur_reference_framerate() {
+		return motion_blur_reference_framerate;
+	}
+
+	_FORCE_INLINE_ RSE::MotionBlurQuality camera_attributes_get_motion_blur_quality() {
+		return motion_blur_quality;
+	}
+
+	_FORCE_INLINE_ RSE::MotionBlurTileSize camera_attributes_get_motion_blur_tile_size() {
+		return motion_blur_tile_size;
 	}
 
 	_FORCE_INLINE_ RSE::DOFBlurQuality camera_attributes_get_dof_blur_quality() {